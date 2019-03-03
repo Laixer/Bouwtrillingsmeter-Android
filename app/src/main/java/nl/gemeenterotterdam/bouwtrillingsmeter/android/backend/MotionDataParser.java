3fb326263e9b75f0925d28f11b6f41fd36d26f13
thomasbeckers <tabeckers95@gmail.com>
2019-03-03 13:37:51 +0100

    Commented out MotionDataParser

--- app/src/main/java/nl/gemeenterotterdam/bouwtrillingsmeter/android/backend/MotionDataParser.java
+++ app/src/main/java/nl/gemeenterotterdam/bouwtrillingsmeter/android/backend/MotionDataParser.java
@@ -13,65 +13,65 @@ package nl.gemeenterotterdam.bouwtrillingsmeter.android.backend;
  */
 class MotionDataParser {
 
-    public static DataCollection currentDataCollection;
-
-    /**
-     * This starts the collection of data.
-     * TODO Create a bulletproof way of getting sensor access.
-     */
-    public static void startDataCollection() {
-        // Check all edge cases where we can not start collecting data
-        if (!SensorControl.getHasSensorAccess()) {
-            System.out.println("We do not have access to the sensors.");
-            return;
-        }
-
-        // If we are ready to go
-        currentDataCollection = new DataCollection();
-    }
-
-    /**
-     * This will close our DataCollection object.
-     * This will also stop the calculation loop.
-     */
-    public static void endDataCollection() {
-        if (null == currentDataCollection) {
-            System.out.println("No data collection was initialized.");
-        }
-
-        if (currentDataCollection.isCurrentlyCollectingData()) {
-            currentDataCollection.onStopCollectionOfData();
-        } else {
-            System.out.println("Current data collection has already ended.");
-        }
-    }
-
-    private static void updateFFT() {
-
-    }
-
-    private static void updateFrequency() {
-
-    }
-
-    /**
-     * This calculates the dominant frequencies on a given interval
-     * TODO Calculate
-     *
-     * @return A DominantFrequencies object with the calculated frequencies
-     */
-    private static DominantFrequencies calculateDominantFrequencies() {
-        DominantFrequencies result = null;
-
-        return result;
-    }
-
-    private static void velocityInFrequncyDomain() {
-
-    }
-
-    private static void getLimitValue() {
-
-    }
+//    public static DataCollection currentDataCollection;
+//
+//    /**
+//     * This starts the collection of data.
+//     * TODO Create a bulletproof way of getting sensor access.
+//     */
+//    public static void startDataCollection() {
+//        // Check all edge cases where we can not start collecting data
+//        if (!SensorControl.getHasSensorAccess()) {
+//            System.out.println("We do not have access to the sensors.");
+//            return;
+//        }
+//
+//        // If we are ready to go
+//        currentDataCollection = new DataCollection();
+//    }
+//
+//    /**
+//     * This will close our DataCollection object.
+//     * This will also stop the calculation loop.
+//     */
+//    public static void endDataCollection() {
+//        if (null == currentDataCollection) {
+//            System.out.println("No data collection was initialized.");
+//        }
+//
+//        if (currentDataCollection.isCurrentlyCollectingData()) {
+//            currentDataCollection.onStopCollectionOfData();
+//        } else {
+//            System.out.println("Current data collection has already ended.");
+//        }
+//    }
+//
+//    private static void updateFFT() {
+//
+//    }
+//
+//    private static void updateFrequency() {
+//
+//    }
+//
+//    /**
+//     * This calculates the dominant frequencies on a given interval
+//     * TODO Calculate
+//     *
+//     * @return A DominantFrequencies object with the calculated frequencies
+//     */
+//    private static DominantFrequencies calculateDominantFrequencies() {
+//        DominantFrequencies result = null;
+//
+//        return result;
+//    }
+//
+//    private static void velocityInFrequncyDomain() {
+//
+//    }
+//
+//    private static void getLimitValue() {
+//
+//    }
 
 }
