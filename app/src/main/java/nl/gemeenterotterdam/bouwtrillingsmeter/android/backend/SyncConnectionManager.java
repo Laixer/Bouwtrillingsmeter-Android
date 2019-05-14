adeac938dc515080d142614c357ed2551693b74a
thomasbeckers <tabeckers95@gmail.com>
2019-05-14 17:55:01 +0200

    First communication with local node server is live!

--- app/src/main/java/nl/gemeenterotterdam/bouwtrillingsmeter/android/backend/SyncConnectionManager.java
+++ app/src/main/java/nl/gemeenterotterdam/bouwtrillingsmeter/android/backend/SyncConnectionManager.java
@@ -8,6 +8,13 @@ import android.net.NetworkInfo;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -16,11 +23,55 @@ import java.util.ArrayList;
  */
 class SyncConnectionManager {
 
+    private static final String urlString = "http://192.168.0.40:3000/poster";
+
     /**
      * Initializes the instance
      */
     static void initialize() {
+        JSONObject object = JSONCompiler.compileUserUID("this is my user UID");
+        post(object);
+    }
 
+    /**
+     * This posts to the server.
+     * TODO Uses the mock api now
+     *
+     * @param object The JSON object to send
+     */
+    private static void post(JSONObject object) {
+        Thread thread = new Thread(() -> {
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json; utf-8");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoOutput(true);
+
+                OutputStream outputStream = connection.getOutputStream();
+                byte[] input = object.toString().getBytes();
+                outputStream.write(input, 0, input.length);
+
+                // Read response
+                InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream(), "utf-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = bufferedReader.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+
+            } catch (MalformedURLException e) {
+                System.out.println(e.toString());
+            } catch (IOException e) {
+                System.out.println(e.toString());
+            }
+        });
+        thread.start();
     }
 
     /**
@@ -57,7 +108,7 @@ class SyncConnectionManager {
     /**
      * Push data interval essentials
      *
-     * @param measurementUID            The measurement UID
+     * @param measurementUID             The measurement UID
      * @param dataIntervalEssentialsList All the data interval essentials
      * @return True if successful
      */
@@ -87,7 +138,7 @@ class SyncConnectionManager {
      * Push an image to the API.
      *
      * @param measurementUID The measurement UID that belongs to the image
-     * @param image The image
+     * @param image          The image
      * @return True if successful
      */
     static boolean pushImage(String measurementUID, Image image) {
